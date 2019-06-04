ee88cc3d850f2378a6aa3dbd7ec7fafd7e895610
Mike Brasher <mikbras@microsoft.com>
2019-06-03 20:22:56 -0500

    remove unused functions.

--- enclave/core/printf.c
+++ enclave/core/printf.c
@@ -718,25 +718,3 @@ int oe_printf(const char* format, ...)
 
     return n;
 }
-
-int oe_puts(const char* s)
-{
-    oe_printf("%s\n", s);
-    return 0;
-}
-
-int oe_putchar(int c)
-{
-    oe_printf("%c", c);
-    return 0;
-}
-
-__attribute__((weak)) int putchar(int c)
-{
-    return oe_putchar(c);
-}
-
-__attribute__((weak)) int puts(const char* s)
-{
-    return oe_puts(s);
-}
